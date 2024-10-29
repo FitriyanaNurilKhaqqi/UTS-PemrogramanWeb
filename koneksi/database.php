8e8058a5e7b3294b33a9842e1123db642f14273c
FitriyanaNurilKhaqqi <128881458+FitriyanaNurilKhaqqi@users.noreply.github.com>
2024-10-29 13:56:30 +0700

    Add files via upload

--- koneksi/database.php
+++ koneksi/database.php
@@ -1,1 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "portfolio";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+  }
+  ?>
